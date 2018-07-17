cf328aa3ce21b6924222684e20efcc4b9f0f7675
Lucas Gomes <gomes@Junior-MacBook.local>
2018-07-17 14:10:20 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+//IMPORTA O LINK DA PAGE PARA PODERMOS USAR LA EM BAIXO
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { IntroPageModule } from '../pages/intro/intro.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    //IMPORTA AS CONFIGURACOES DA PAGINA PARA O APP
+    FeedPageModule,
+    IntroPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
